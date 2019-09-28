8af460077bc178564424b8b366d5b12cf053f237
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2019-09-28 22:36:37 +0200

    completed

---
+++ hiderCommand.js
@@ -0,0 +1,5 @@
+//hidding top viewed items on the market which have the unwanted images.
+document.getElementById("pagelet_marketplace_new_user_top_picks_rhc").style.display = "none"; 
+
+//hidding recent viewed items on the market -just if you want to hide everything :) - .
+document.getElementById("pagelet_marketplace_recently_viewed_rhc").style.display = "none"; 
